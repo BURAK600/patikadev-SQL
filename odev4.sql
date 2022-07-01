5018dabdcb0f8b0d854f82dc8f0c7e08e9b1c585
burak <72358222+BURAK600@users.noreply.github.com>
2022-07-02 00:23:20 +0300

    Create odev4.sql

---
+++ odev4.sql
@@ -0,0 +1,13 @@
+select distinct replacement_cost from film;
+
+
+select count(distinct replacement_cost) from film;
+
+
+select title from film where title like 'T%' and rating = 'G';
+
+
+select country from country where country like '_____';
+
+
+select city from city where city ilike '%r';
